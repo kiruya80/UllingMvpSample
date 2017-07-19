4683b47e14fa2a70e324e5be76b24e507399bea7
kiruya80 <kiruya80@gmail.com>
2017-07-19 18:00:41 +0900

    현재 페이지 확인 수정

--- ullinglibcore/src/main/java/com/ulling/lib/core/base/QcBaseShowLifeFragement.java
+++ ullinglibcore/src/main/java/com/ulling/lib/core/base/QcBaseShowLifeFragement.java
@@ -5,8 +5,6 @@ import android.view.View;
 
 import com.ulling.lib.core.util.QcLog;
 
-import static com.ulling.lib.core.util.QcLog.i;
-
 /**
  * 현재 보이는 프레그먼트
  * 보이는 경우
@@ -31,19 +29,28 @@ public abstract class QcBaseShowLifeFragement extends QcBaseLifeFragment {
     }
 
     @Override
+    public void onPause() {
+        super.onPause();
+        hasShowData = false;
+        isViewPrepared = false;
+    }
+
+    @Override
     public void setUserVisibleHint(boolean isVisibleToUser) {
         super.setUserVisibleHint(isVisibleToUser);
-        i("setUserVisibleHint == " + section_number + " , " + isVisibleToUser);
+//        if (getArguments() != null && section_number < 0) {
+//            section_number = getArguments().getInt(ARG_SECTION_NUMBER);
+//            QcLog.e( "setUserVisibleHint getArguments()  != null = " + section_number);
+//        }
         /**
          * QcBaseLifeFragment 의 resume에서 애니메이션 플레그 막기
          */
         isResumeAnimation = false;
-
-        if (isVisibleToUser) {
-            // 현재 페이지
-            showToUserIfPrepared();
+        QcLog.i( "setUserVisibleHint == " + section_number + " , " + isVisibleToUser + " ," + isViewPrepared);
+        if (isVisibleToUser && isViewPrepared) {
+            hasShowData = true;
+            needShowToUser();
         } else {
-            // 감춰진다
             hasShowData = false;
             optAnimationPause();
         }
@@ -52,11 +59,13 @@ public abstract class QcBaseShowLifeFragement extends QcBaseLifeFragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        QcLog.i("onViewCreated == ");
+        QcLog.i( "onViewCreated == " + section_number + " ," + getUserVisibleHint());
         isViewPrepared = true;
-        showToUserIfPrepared();
+        if (!hasShowData && getUserVisibleHint()) {
+            hasShowData = true;
+            needShowToUser();
+        }
     }
-
 
     @Override
     protected void optGetArgument(Bundle savedInstanceState) {
@@ -68,12 +77,42 @@ public abstract class QcBaseShowLifeFragement extends QcBaseLifeFragment {
         }
     }
 
-    private void showToUserIfPrepared() {
-        if (getUserVisibleHint() && !hasShowData && isViewPrepared) {
-            hasShowData = true;
-            needShowToUser();
-            optAnimationResume();
-        }
-    }
+
+//    @Override
+//    public void setUserVisibleHint(boolean isVisibleToUser) {
+//        super.setUserVisibleHint(isVisibleToUser);
+//        i("setUserVisibleHint == " + section_number + " , " + isVisibleToUser);
+//        /**
+//         * QcBaseLifeFragment 의 resume에서 애니메이션 플레그 막기
+//         */
+//        isResumeAnimation = false;
+//
+//        if (isVisibleToUser) {
+//            // 현재 페이지
+//            showToUserIfPrepared();
+//        } else {
+//            // 감춰진다
+//            hasShowData = false;
+//            optAnimationPause();
+//        }
+//    }
+//
+//    @Override
+//    public void onViewCreated(View view, Bundle savedInstanceState) {
+//        super.onViewCreated(view, savedInstanceState);
+//        QcLog.i("onViewCreated == ");
+//        isViewPrepared = true;
+//        showToUserIfPrepared();
+//    }
+//
+//
+//
+//    private void showToUserIfPrepared() {
+//        if (getUserVisibleHint() && !hasShowData && isViewPrepared) {
+//            hasShowData = true;
+//            needShowToUser();
+//            optAnimationResume();
+//        }
+//    }
 
 }
